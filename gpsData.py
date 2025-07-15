09b4b569ace5efe9773273c88dd90f2e04beca32
ParkerQH <blueskyaoa7@gmail.com>
2025-07-15 14:43:46 +0900

    gps 모듈 분리

---
+++ gpsData.py
@@ -0,0 +1,33 @@
+import os
+
+api_key = os.getenv("VWorld_API")
+
+def gps(gps_info):
+    lat_str, lon_str = gps_info.strip().split()
+    lat = float(lat_str)
+    lon = float(lon_str)
+    parcel_addr = reverse_geocode(lat, lon, api_key)
+
+
+def reverse_geocode(lat, lon, api_key):
+    url = "https://api.vworld.kr/req/address"
+    params = {
+        "service": "address",
+        "request": "getAddress",
+        "crs": "epsg:4326",
+        "point": f"{lon},{lat}",
+        "format": "json",
+        "type": "parcel",
+        "key": api_key,
+    }
+    response = requests.get(url, params=params)
+
+    # 반환값 단순화
+    if response.status_code == 200:
+        data = response.json()
+        if data["response"]["status"] == "OK":
+            # 첫 번째 결과에서 지번주소 추출
+            result = data["response"]["result"][0]
+            if "text" in result:
+                return result["text"]  # 지번주소만 반환
+    return None
